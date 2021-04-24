b5d3f2d6a55a3b32767c312f71c339b44b13bfac
Elizandra Rocha <rochaelizandraf@gmail.com>
2021-04-24 18:10:16 -0400

    ajustar: imprimir somente 1 casa decimal (a saída)

---
+++ situacaoAluno/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        float nota1 = entrada.nextFloat();
+        float nota2 = entrada.nextFloat();
+        float nota3 = entrada.nextFloat();
+        int faltas = entrada.nextInt();
+
+        float media = (nota1 + nota2 + nota3)/3;
+
+        if(media >= 7.0 && faltas <= 17) System.out.println("Aprovado " + media);
+        if(faltas > 17) System.out.println("Reprovado por falta");
+        if(media < 7.0) System.out.println("Reprovado por nota " + media);
+
+    }
+}
