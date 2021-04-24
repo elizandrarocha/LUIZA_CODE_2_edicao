de04cf0db33152dcc24880d3719076d25762a5ba
Elizandra Rocha <rochaelizandraf@gmail.com>
2021-04-24 13:06:27 -0400

    calculo de idades somente em dias done

--- exerciciosFixacaoSabado/src/com/company/Main.java
+++ exerciciosFixacaoSabado/src/com/company/Main.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// escreva um algoritmo para ler um valor (do teclado) e escrever (na tela) o seu antecessor
+
+        /*
+	    // escreva um algoritmo para ler um valor (do teclado) e escrever (na tela) o seu antecessor
         //caso: valores INTEIROS
         Scanner entrada = new Scanner(System.in);
         int valor = entrada.nextInt();
@@ -19,5 +21,20 @@ public class Main {
         float altura = input.nextFloat();
         float area = base*altura;
         System.out.println("A área do retângulo é: " + area);
+         */
+
+        //faça um algoritmo que leia a idade expressa em anos, meses e dias
+        //e escreva a idade dessa pessoa escrita apenas em dias
+        Scanner inputB = new Scanner(System.in);
+        System.out.println("Digite suas idade no formato: ANOS MESES DIAS");
+        int anos = inputB.nextInt();
+        int meses = inputB.nextInt();
+        int dias = inputB.nextInt();
+        int idadeEmDias = 365*anos + 30*meses + dias;
+        System.out.println("Sua idade em dias é: " + idadeEmDias);
+
+
+
+
     }
 }
